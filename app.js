d8c312126a90055d1de7fa608b9ef834621b95c5
kjonilonis <k.jonilonis.2309@westcliff.edu>
2026-02-25 21:20:36 -0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Full DevOps Pipeline Working - Student 4 🚀");
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
